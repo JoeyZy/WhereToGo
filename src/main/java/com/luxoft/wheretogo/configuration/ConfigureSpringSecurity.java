42ed75e5a2710631d6ac2debc7412f625fc422d8
Lrakulka <olecsandr32@gmail.com>
2016-07-06 17:41:13 +0300

    Added Spring Security. Authorization not configured.

---
+++ src/main/java/com/luxoft/wheretogo/configuration/ConfigureSpringSecurity.java
@@ -0,0 +1,31 @@
+package com.luxoft.wheretogo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by sasha on 06.07.16.
+ */
+@Configuration
+@EnableWebSecurity
+public class ConfigureSpringSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("mkyong").password("123456").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+                .and().formLogin().loginPage("/login").permitAll();
+
+    }
+}
--- src/main/java/com/luxoft/wheretogo/controller/RestServiceController.java
+++ src/main/java/com/luxoft/wheretogo/controller/RestServiceController.java
@@ -12,10 +12,8 @@ import com.luxoft.wheretogo.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -119,7 +117,22 @@ public class RestServiceController {
 		return usersService.findByEmail(user.getEmail());
 	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+		return model;
+	}
+
+	/*@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public User login(@RequestBody User user, HttpServletRequest request) {
 		User sessionUser = usersService.findByEmail(user.getEmail());
 		if (sessionUser == null || !sessionUser.getPassword().equals(user.getPassword())) {
@@ -128,7 +141,7 @@ public class RestServiceController {
 		request.getSession().setAttribute("user", sessionUser);
 
 		return sessionUser;
-	}
+	}*/
 
 	@RequestMapping(value = "/assignEventToUser", method = RequestMethod.POST)
 	public Event assignUserToEvent(@RequestBody Event event, HttpServletRequest request) {
