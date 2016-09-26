de289f423f8d21f9c70f32fbccb2f9793e12f643
KlymSerhii <klym.serhiy@gmail.com>
2016-09-26 15:37:55 +0300

    fixed bug of a hover of group location(when creating a group)

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -2214,6 +2214,7 @@ $(window).on("load",function () {
 			}
 			else {
 				$('div.GroupPage__groups__list.group_participants div').remove();
+				$groupLocation.attr('title','');
 				makeGroupPageEditable();
 				$buttonAddGroup.show();
 				$groupPageParticipants.hide();
